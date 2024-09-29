f540017ac1e5f515c307f023d15d34226344a236
diala2 <112942959+diala2@users.noreply.github.com>
2024-09-29 19:10:07 +0300

    Initial Commit

---
+++ Balance/BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceApp.swift
+//  Balance
+//
+//  Created by Diala Abdulnasser Fayoumi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Balance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Balance
+//
+//  Created by Diala Abdulnasser Fayoumi on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
